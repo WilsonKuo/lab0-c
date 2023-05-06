a6b593011a57b9d31839ea5b507af82af92860d9
Wilson Kuo <WilsonKuo85@gmail.com>
2023-05-06 16:08:11 +0800

    Implement free command

--- queue.c
+++ queue.c
@@ -22,7 +22,17 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *l) {}
+void q_free(struct list_head *l)
+{
+    if (!list_empty(l)) {
+        element_t *entry, *safe;
+        list_for_each_entry_safe (entry, safe, l, list) {
+            list_del(&entry->list);
+            q_release_element(entry);
+        }
+    }
+    free(l);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
